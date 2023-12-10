dad275428b807fcced913e915a4d1985655cb47c
YALLA PRABHAKARA RAO <42337157+prabhakaryalla@users.noreply.github.com>
2023-12-10 20:39:52 +0530

    Update index.js

--- src/components/player-info/index.js
+++ src/components/player-info/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import players1 from "../../players.json";
 export default function PlayerDetail({ close, i, addPlayer, selectedPlayers }) {
 	const [players] = React.useState([...players1]);
+	const isdisabled = selectedPlayers.find(x => x.name === players[i].name);
 	return (
 		//Style fixed to center
 		<div
@@ -14,31 +15,31 @@ export default function PlayerDetail({ close, i, addPlayer, selectedPlayers }) {
 				width: "500px",
 				top: "30%",
 			}}
-			data-testid={`player-Rohit-Sharma-details`}
+			data-testid={"player-" + players[i].name.split(" ").join("-") + "-details"}
 		>
 			<h1 className="card-title" style={{ textAlign: "center" }}>
 				Player Detail
 			</h1>
 			<p>
-				<strong>Name:</strong> <span data-testid="player-detail-Rohit-Sharma-name">Rohit Sharma</span>
+				<strong>Name:</strong> <span data-testid={"player-detail-" + players[i].name.split(" ").join("-") + "-name"}>{players[i].name}</span>
 			</p>
 			<p>
-				<strong>Type:</strong> <span data-testid="player-detail-Rohit-Sharma-type">Batsman</span>
+				<strong>Type:</strong> <span data-testid={"player-detail-" + players[i].name.split(" ").join("-") + "-type"}>{players[i].type}</span>
 			</p>
 			<p>
-				<strong>Batting:</strong> <span data-testid="player-detail-Rohit-Sharma-batting">90</span>
+				<strong>Batting:</strong> <span data-testid={"player-detail-" + players[i].name.split(" ").join("-") + "-batting"}>{players[i].battingSkill}</span>
 			</p>
 			<p>
-				<strong>Bowling:</strong> <span data-testid="player-detail-Rohit-Sharma-bowling">90</span>
+				<strong>Bowling:</strong> <span data-testid={"player-detail-" + players[i].name.split(" ").join("-") + "-bowling"}>{players[i].bowlingSkill}</span>
 			</p>
 			<button
-				disabled={false}
-				onClick={() => addPlayer(0)}
-				data-testid="player-detail-Rohit-Sharma-add"
+				disabled={isdisabled}
+				onClick={() => addPlayer(i)}
+				data-testid={"player-detail-" + players[i].name.split(" ").join("-") + "-add"}
 			>
 				Select
 			</button>
-			<button onClick={close} className="danger" data-testid="player-detail-Rohit-Sharma-close">
+			<button onClick={close} className="danger" data-testid={"player-detail-" + players[i].name.split(" ").join("-") + "-close"}>
 				Close
 			</button>
 		</div>
